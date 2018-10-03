90c2fa2c1c056d050690585cef18c23251aa5c97
Remi Caumette <rcaumett@z2r2p1.le-101.fr>
2018-10-03 19:13:23 +0200

    update

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: rcaumett <rcaumett@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/03 16:02:16 by rcaumett     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/03 17:15:40 by rcaumett    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/03 19:08:49 by rcaumett    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -25,6 +25,6 @@ int	main(int argc, char **argv)
 		int fd = open(argv[1], O_RDONLY);
 		int i;
 		while ((i = get_next_line(fd, &str)) > 0)
-			printf("%d %d\n", i, strcmp(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempusLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempusLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus"));
+			printf("%d %s\n", i, str);
 	}
 }
