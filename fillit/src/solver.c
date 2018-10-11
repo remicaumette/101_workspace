1c2d33387c07c64cc5136c089f0fc5ff2aeb4af2
Remi Caumette <rcaumett@z2r3p2.le-101.fr>
2018-10-11 19:00:42 +0200

    update

--- fillit/src/solver.c
+++ fillit/src/solver.c
@@ -6,7 +6,7 @@
 /*   By: rcaumett <rcaumett@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/09 13:05:43 by jarcher      #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/10 16:39:45 by jarcher     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/11 18:59:51 by rcaumett    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -38,10 +38,12 @@ t_uint8	can_place(t_fillit *fillit, t_tet *tet, t_uint8 map_x, t_uint8 map_y)
 
 	i = -1;
 	while (++i < 16)
+	{
 		if (get_tetriminos_bit(tet, i / 4, i % 4) &&
 				(i / 4 + map_y >= fillit->size || i % 4 + map_x >= fillit->size ||
 				fillit->map[map_x + i % 4 + (map_y + i / 4) * fillit->size]))
 			return (0);
+	}
 	return (1);
 }
 
