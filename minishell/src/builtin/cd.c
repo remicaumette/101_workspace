60ddcc8e3bed122d3d4b16de9504fcc7322b676c
Remi Caumette <rcaumett@z3r5p1.le-101.fr>
2018-12-12 18:27:42 +0100

    update

--- minishell/src/builtin/cd.c
+++ minishell/src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: rcaumett <rcaumett@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/12/06 15:34:09 by rcaumett     #+#   ##    ##    #+#       */
-/*   Updated: 2018/12/12 17:21:37 by rcaumett    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/12/12 18:27:17 by rcaumett    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -28,6 +28,7 @@ int			builtin_cd(t_shell *shell, t_cmd *cmd)
 	int			len;
 	struct stat	stats;
 	char		*dir;
+	DIR			*direc;
 
 	if ((len = ft_strarr_len(cmd->args)) > 1)
 		return (minishell_printerr("cd", "Too many arguments"));
@@ -36,7 +37,8 @@ int			builtin_cd(t_shell *shell, t_cmd *cmd)
 		return (file_error(dir, "No such file or directory"));
 	if (!S_ISDIR(stats.st_mode))
 		return (file_error(dir, "Not a directory"));
-	if (!opendir(dir))
+	if (!(direc = opendir(dir)))
 		return (file_error(dir, "Permission denied"));
+	closedir(direc);
 	return (!!chdir(dir));
 }
