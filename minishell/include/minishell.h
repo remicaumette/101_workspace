1cb0569447598d47fb4cd54328ef0d2ad01d41d7
Remi Caumette <rcaumett@z3r5p1.le-101.fr>
2018-12-13 14:47:23 +0100

    cleanup

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   shelltte.h                                    .::    .:/ .      .::   */
+/*   minishell.h                                      .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: rcaumett <rcaumett@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/11/02 19:07:50 by rcaumett     #+#   ##    ##    #+#       */
-/*   Updated: 2018/12/04 14:04:08 by rcaumett    ###    #+. /#+    ###.fr     */
+/*   Created: 2018/12/13 14:38:08 by rcaumett     #+#   ##    ##    #+#       */
+/*   Updated: 2018/12/13 14:43:11 by rcaumett    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,9 +18,9 @@
 # include <sys/stat.h>
 # include <dirent.h>
 
-typedef struct s_cmd		t_cmd;
-typedef struct s_shell		t_shell;
-typedef struct s_word		t_word;
+typedef struct s_cmd	t_cmd;
+typedef struct s_shell	t_shell;
+typedef struct s_word	t_word;
 typedef int		(*t_builtin)(t_shell *, t_cmd *);
 
 struct					s_cmd
@@ -50,30 +50,30 @@ struct					s_word
 	char	*tmp;
 };
 
-int							shell_init(t_shell *shell, char **env);
-void						shell_destroy(t_shell *shell);
-int							shell_eval(t_shell *shell);
-char						*shell_getenv(t_shell *shell, char *name);
+int						shell_init(t_shell *shell, char **env);
+void					shell_destroy(t_shell *shell);
+int						shell_eval(t_shell *shell);
+char					*shell_getenv(t_shell *shell, char *name);
 
-int							cmd_from_line(t_shell *shell);
-t_cmd						*cmd_from_words(char **words);
-void						cmd_destroy(t_cmd **cmd);
-int							cmd_resolve_exec(t_shell *shell, t_cmd *cmd);
+int						cmd_from_line(t_shell *shell);
+t_cmd					*cmd_from_words(char **words);
+void					cmd_destroy(t_cmd **cmd);
+int						cmd_resolve_exec(t_shell *shell, t_cmd *cmd);
 
-int							words_from_line(t_shell *shell, char ***words);
+int						words_from_line(t_shell *shell, char ***words);
 
-char						*process_getexec(t_shell *shell, char *file);
-int							process_run(t_shell *shell, t_cmd *cmd);
+char					*process_getexec(t_shell *shell, char *file);
+int						process_run(t_shell *shell, t_cmd *cmd);
 
-int							builtin_cd(t_shell *shell, t_cmd *cmd);
-int							builtin_echo(t_shell *shell, t_cmd *cmd);
-int							builtin_env(t_shell *shell, t_cmd *cmd);
-int							builtin_exit(t_shell *shell, t_cmd *cmd);
-int							builtin_setenv(t_shell *shell, t_cmd *cmd);
-int							builtin_unsetenv(t_shell *shell, t_cmd *cmd);
+int						builtin_cd(t_shell *shell, t_cmd *cmd);
+int						builtin_echo(t_shell *shell, t_cmd *cmd);
+int						builtin_env(t_shell *shell, t_cmd *cmd);
+int						builtin_exit(t_shell *shell, t_cmd *cmd);
+int						builtin_setenv(t_shell *shell, t_cmd *cmd);
+int						builtin_unsetenv(t_shell *shell, t_cmd *cmd);
 
-t_builtin					builtin_from_name(char *cmd);
-int							minishell_printerr(char *file, char *msg);
-char						*ft_strjoinc(char **mot, char c);
-char						*path_join(char *path, char *filename);
+t_builtin				builtin_from_name(char *cmd);
+int						minishell_printerr(char *file, char *msg);
+char					*ft_strjoinc(char **mot, char c);
+char					*path_join(char *path, char *filename);
 #endif
